bf7a6992a14f6583ec258b823c15005ddbc67139
ayushdumka204 <ayushdumka2005@gmail.com>
2026-03-28 14:59:28 +0530

    interview api

--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -1,42 +1,27 @@
 const express = require("express")
-const authMiddleware = require("../middlewares/auth.middleware")
 const interviewController = require("../controllers/interview.controller")
 const upload = require("../middlewares/file.middleware")
 
 const interviewRouter = express.Router()
 
-
-
 /**
  * @route POST /api/interview/
- * @description generate new interview report on the basis of user self description,resume pdf and job description.
- * @access private
  */
-interviewRouter.post("/", authMiddleware.authUser, upload.single("resume"), interviewController.generateInterViewReportController)
+interviewRouter.post("/", upload.single("resume"), interviewController.generateInterViewReportController)
 
 /**
  * @route GET /api/interview/report/:interviewId
- * @description get interview report by interviewId.
- * @access private
  */
-interviewRouter.get("/report/:interviewId", authMiddleware.authUser, interviewController.getInterviewReportByIdController)
-
+interviewRouter.get("/report/:interviewId", interviewController.getInterviewReportByIdController)
 
 /**
  * @route GET /api/interview/
- * @description get all interview reports of logged in user.
- * @access private
  */
-interviewRouter.get("/", authMiddleware.authUser, interviewController.getAllInterviewReportsController)
-
+interviewRouter.get("/", interviewController.getAllInterviewReportsController)
 
 /**
- * @route GET /api/interview/resume/pdf
- * @description generate resume pdf on the basis of user self description, resume content and job description.
- * @access private
+ * @route POST /api/interview/resume/pdf/:interviewReportId
  */
-interviewRouter.post("/resume/pdf/:interviewReportId", authMiddleware.authUser, interviewController.generateResumePdfController)
-
-
+interviewRouter.post("/resume/pdf/:interviewReportId", interviewController.generateResumePdfController)
 
 module.exports = interviewRouter
