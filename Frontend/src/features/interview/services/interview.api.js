567524fe09e764abd636f38a122d6194b528040a
ayushdumka204 <ayushdumka2005@gmail.com>
2026-03-28 12:12:36 +0530

    base url interview

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-baseURL: "http://localhost:3000",
-    withCredentials: true
+  baseURL: "https://gen-ai-model.onrender.com", // ✅ change yaha
+  withCredentials: true
 })
 
 /**
