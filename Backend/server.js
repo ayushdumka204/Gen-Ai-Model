002cfb9736ed2ed73ce8e2342fa33520b7aadf14
ayushdumka204 <ayushdumka2005@gmail.com>
2026-03-28 12:30:05 +0530

    port from 3000 to production

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,14 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+require("dotenv").config();
 
-connectToDB()
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
+// connect DB
+connectToDB();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+// dynamic port (VERY IMPORTANT)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
