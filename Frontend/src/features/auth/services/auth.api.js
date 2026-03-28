63466ca0f8a682a94ed2c083bced4a96c50bd1e6
ayushdumka204 <ayushdumka2005@gmail.com>
2026-03-28 12:11:03 +0530

    base url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,52 +1,52 @@
 import axios from "axios"
 
 const api = axios.create({
-baseURL: "http://localhost:3000",
-    withCredentials: true
+  baseURL: "https://gen-ai-model.onrender.com", // ✅ change yaha
+  withCredentials: true
 })
 
 export async function register({ username, email, password }) {
-    try {
-        const response = await api.post('/api/auth/register', {
-            username, email, password
-        })
-        return response.data
-    } catch (err) {
-        console.error("Register API error", err)
-        const message = err?.response?.data?.message || err?.message || "Failed to register"
-        throw new Error(message)
-    }
+  try {
+    const response = await api.post('/api/auth/register', {
+      username, email, password
+    })
+    return response.data
+  } catch (err) {
+    console.error("Register API error", err)
+    const message = err?.response?.data?.message || err?.message || "Failed to register"
+    throw new Error(message)
+  }
 }
 
 export async function login({ email, password }) {
-    try {
-        const response = await api.post("/api/auth/login", {
-            email, password
-        })
-        return response.data
-    } catch (err) {
-        console.error("Login API error", err)
-        const message = err?.response?.data?.message || err?.message || "Failed to login"
-        throw new Error(message)
-    }
+  try {
+    const response = await api.post("/api/auth/login", {
+      email, password
+    })
+    return response.data
+  } catch (err) {
+    console.error("Login API error", err)
+    const message = err?.response?.data?.message || err?.message || "Failed to login"
+    throw new Error(message)
+  }
 }
 
 export async function logout() {
-    try {
-        const response = await api.get("/api/auth/logout")
-        return response.data
-    } catch (err) {
-        console.error("Logout API error", err)
-        throw err
-    }
+  try {
+    const response = await api.get("/api/auth/logout")
+    return response.data
+  } catch (err) {
+    console.error("Logout API error", err)
+    throw err
+  }
 }
 
 export async function getMe() {
-    try {
-        const response = await api.get("/api/auth/get-me")
-        return response.data
-    } catch (err) {
-        console.error("GetMe API error", err)
-        throw err
-    }
+  try {
+    const response = await api.get("/api/auth/get-me")
+    return response.data
+  } catch (err) {
+    console.error("GetMe API error", err)
+    throw err
+  }
 }
